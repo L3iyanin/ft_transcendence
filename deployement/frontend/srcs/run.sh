564a85b41b84e4b583f2986e57708ac4e253f5d6
Imran Baali <ibaali@e1r6p6.1337.ma>
2022-09-10 23:06:41 +0100

    start making npm install in build time of container not in run time

--- deployement/frontend/srcs/run.sh
+++ deployement/frontend/srcs/run.sh
@@ -1,3 +1,7 @@
-rm -rf node_modules
-npm install
+if [ ! -d "/ft_transcendence_frontend/node_modules" ]
+then
+	mv /node_modules /ft_transcendence_frontend/node_modules
+	rm -rf /package.json
+fi
+
 npm start
