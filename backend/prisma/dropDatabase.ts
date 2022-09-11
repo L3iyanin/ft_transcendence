19f3c58202cdde9791c37c4f67e30fca9ed90171
Khalid Benlyazid <kbenlyaz@e1r6p4.1337.ma>
2022-09-11 17:07:44 +0100

    feat: add seed and drop cmd  to package.json and add default value to some user field

---
+++ backend/prisma/dropDatabase.ts
@@ -0,0 +1,26 @@
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.deleteMany()
+    const achievement = await prisma.achievement.deleteMany()
+    const match = await prisma.match.deleteMany()
+    const channel = await prisma.chennel.deleteMany()
+    const member = await prisma.member.deleteMany()
+    const message = await prisma.message.deleteMany()
+    console.log({user, achievement, match, channel, member, message})
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
---
+++ backend/prisma/seed.ts
@@ -0,0 +1,68 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function addAchivements(){
+    const achivement0 = await prisma.achievement.create({
+      data : {
+          name: 'achivement0',
+          description: 'achivement0 desc',
+          imgUrl: '/home/image0.jpg'
+      }
+    
+    });
+    const achivement1 = await prisma.achievement.create({
+      data : {
+          name: 'achivement1',
+          description: 'achivement1 desc',
+          imgUrl: '/home/image1.jpg'
+      }
+    
+    });
+    console.log({ achivement0, achivement1});
+}
+
+async function addUsers(){
+    const user0 = await prisma.user.create({
+        data : {
+            username : "user0",
+            fullName : "fdfdff",
+            imgUrl :  "/path/image/profile"
+        }
+    })
+    const user1 = await prisma.user.create({
+        data : {
+            username : "user1",
+            fullName : "cdcdcd",
+            imgUrl :  "/path/image/profile"
+        }
+    })
+    const user2 = await prisma.user.create({
+        data : {
+            username : "user2",
+            fullName : "fdfdfd",
+            imgUrl :  "/path/image/profile"
+        }
+    })
+    console.log({user0, user1, user2})
+}
+
+async function main() {
+    await addAchivements()
+    await addUsers()
+
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
