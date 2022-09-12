86ff708fb5cbc3d8940a51f1d784d47d5b2f7512
Khalid Benlyazid <kbenlyaz@e1r6p4.1337.ma>
2022-09-12 12:46:02 +0100

    add global variable path and enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials : true,
+    origin : "http://localhost:3000"
+  });
+
   app.setGlobalPrefix('api');
   const PORT = process.env.PORT || 8080;
   await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
