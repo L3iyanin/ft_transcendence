a4cfeeef4809423a35f3d372750f6a923dadec73
Khalid Benlyazid <kbenlyaz@e1r6p4.1337.ma>
2022-09-12 12:47:45 +0100

    aut user with 42, save it in database then return JWT token

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import {AuthUserData } from './auth.interface';
+import { AuthService } from './auth.service';
+
+interface user{
+
+}
+@Controller('auth/42')
+export class AuthController {
+    constructor(private readonly authService : AuthService){}
+    @Get()
+    async Auth42(@Req() req:Request, @Res() res: Response){
+        const code : any = req.query['code']
+        const userData : AuthUserData  = await this.authService.getUserData(code)
+        const avatarImage = this.authService.getImageProfileUrl(userData.userName)
+        const user = await this.authService.saveUserInDatabase(userData.userName, userData.fullName, avatarImage)
+        const token = await this.authService.crietJwtToken(user.username, user.fullName)
+        res.cookie('jwt', token, {httpOnly : true})
+        return res.send({
+            status : 200,
+            data : userData
+        })
+    }
+}
