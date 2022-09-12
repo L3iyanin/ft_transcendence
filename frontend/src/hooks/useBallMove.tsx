6255761de1c741394b815b2c1630a720f9471471
Imran Baali <ibaali@e1r5p7.1337.ma>
2022-09-12 21:53:08 +0100

    add make ball move

--- frontend/src/hooks/useBallMove.tsx
+++ frontend/src/hooks/useBallMove.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const useBallMove = ({x , y}: {x: number, y: number}) => {
+const useBallMove = ({ x, y }: { x: number; y: number }) => {
 	const [ballPosition, setBallPosition] = useState<{
 		x: number;
 		y: number;
@@ -9,9 +9,26 @@ const useBallMove = ({x , y}: {x: number, y: number}) => {
 		y: y,
 	});
 
+	const requestRef = useRef<null>();
+
+	const animate = (time: any) => {
+		setBallPosition((prev) => {
+			return {
+				x: prev.x + 1,
+				y: prev.y + 1,
+			};
+		});
+		requestRef.current = requestAnimationFrame(animate);
+	};
+
+	useEffect(() => {
+		requestRef.current = requestAnimationFrame(animate);
+		return () => cancelAnimationFrame(requestRef.current!);
+	}, []);
+
 	return {
 		ballPosition,
-	}
+	};
 };
 
 export default useBallMove;
