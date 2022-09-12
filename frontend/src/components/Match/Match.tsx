f91479259ca1deaf9e3b1fd45bb8e2206790ebd8
Younes Arroubi <yarroubi@e1r7p6.1337.ma>
2022-09-12 23:45:24 +0100

    match component in dev #22

--- frontend/src/components/Match/Match.tsx
+++ frontend/src/components/Match/Match.tsx
@@ -8,7 +8,7 @@ const Match = ({children} : {children : IGameMatch}) => {
 	console.log(player1);
 	let player2 = children.player2;
 	return (
-		<div className="container h-8 w-96 flex justify-center items-center border rounded-lg border-beige">
+		<div className="container h-8 w-auto flex justify-center items-center border rounded-lg border-beige">
 			<Player username={player1.username} avatar={player1.imageUrl} side="left" ></Player>
 			<Score score1={player1.score} score2={player2.score} className="bg-yellow"/>
 			<Player username={player2.username} avatar={player2.imageUrl} side="right" ></Player>
--- frontend/src/components/Match/Score.tsx
+++ frontend/src/components/Match/Score.tsx
@@ -1,8 +1,9 @@
 const Score = ({ score1, score2, className}: { score1: number, score2: number, className: string }) => {
 	return (
-		<div className={"flex w-40 rounded-xl justify-center text-center font-medium text-white tracking-superwide " + className}>
+		// <div className={"flex w-40 rounded-xl justify-center text-center font-medium text-white " + className}>
+		<div className={"flex w-40 rounded-xl justify-center text-center font-medium text-white tracking-widest " + className}>
 			<p className="block text-center px-3">
-				{score1 + ":" + score2}
+				{score1 + " : " + score2}
 			</p>
 		</div>
 	);
