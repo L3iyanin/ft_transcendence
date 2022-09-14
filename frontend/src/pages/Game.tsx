371e86493f6a1bb0174d8543884ced86a410afbf
Imran Baali <ibaali@e1r5p5.1337.ma>
2022-09-14 13:57:04 +0100

    fix footer problem in game page

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -6,20 +6,22 @@ import Footer from "../components/Footer/Footer";
 
 const Game: React.FC = () => {
 	return (
-		<div className="container">
-			<div className="mt-20" />
-			<TopSection
-				name={fakeGameSettings.name}
-				goalsToWin={fakeGameSettings.goalsToWin}
-			/>
-			<PlayGround settings={fakeGameSettings} />
-			<WatchersAndPlayers
-				player1={fakeGameSettings.player1}
-				player2={fakeGameSettings.player2}
-				watchers={fakeGameSettings.watchers}
-			/>
+		<>
+			<div className="container">
+				<div className="mt-20" />
+				<TopSection
+					name={fakeGameSettings.name}
+					goalsToWin={fakeGameSettings.goalsToWin}
+				/>
+				<PlayGround settings={fakeGameSettings} />
+				<WatchersAndPlayers
+					player1={fakeGameSettings.player1}
+					player2={fakeGameSettings.player2}
+					watchers={fakeGameSettings.watchers}
+				/>
+			</div>
 			<Footer />
-		</div>
+		</>
 	);
 };
 
