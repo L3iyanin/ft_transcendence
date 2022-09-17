21f4c22faa57ff8e7a85acb827177853f868a154
Imran Baali <ibaali@e1r5p5.1337.ma>
2022-09-17 12:38:16 +0100

    add storing language in cookie

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { CookiesProvider, useCookies } from "react-cookie";
+import { useTranslation } from "react-i18next";
 import Router from "./routes/Router";
+import { LNG } from "./utils/constants/settings";
 
 const App: React.FC = () => {
+
+	const [cookies, setCookie, removeCookie] = useCookies([LNG]);
+
+	const { i18n } = useTranslation();
+
+	useEffect(() => {
+		if (cookies[LNG]) {
+			i18n.changeLanguage(cookies[LNG]);
+		}
+	}, []);
+
 	return (
-		<Router />
+		<CookiesProvider>
+			<Router />
+		</CookiesProvider>
 	);
 };
 
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,18 +1,25 @@
+import { useCookies } from "react-cookie";
 import { useTranslation } from "react-i18next";
+import { LNG } from "../../utils/constants/settings";
 
 const Footer = () => {
 	const { t, i18n } = useTranslation();
 
+	const [cookies, setCookie, removeCookie] = useCookies([LNG]);
+
 	const changeLanguageToEnglish = () => {
 		i18n.changeLanguage("en");
+		setCookie(LNG, "en", { path: "/" });
 	};
 
 	const changeLanguageToArabic = () => {
 		i18n.changeLanguage("ar");
+		setCookie(LNG, "ar", { path: "/" });
 	};
 
 	const changeLanguageToTifinagh = () => {
 		i18n.changeLanguage("ti");
+		setCookie(LNG, "ti", { path: "/"});
 	};
 
 	return (
