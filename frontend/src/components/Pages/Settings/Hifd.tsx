f93af0b4b308314ac1e0ec258c14b7cf62180114
Younes Arroubi <yarroubi@e1r7p6.1337.ma>
2022-09-17 17:23:23 +0100

    settings done #25

--- frontend/src/components/Pages/Settings/Hifd.tsx
+++ frontend/src/components/Pages/Settings/Hifd.tsx
@@ -1,6 +1,6 @@
 const Hifd = () => {
 	return (
-		<button className="container h-52 p-x-20 rounded-lg font-extrabold text-8xl text-white bg-green">{"حِفظ"}</button>
+		<button className="container h-60 p-x-20 rounded-lg font-extrabold text-8xl text-white bg-green">{"حِفظ"}</button>
 	);
 }
 
