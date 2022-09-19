d35f8760765be941033bf555d2221fdad58bdf8a
Younes Arroubi <yarroubi@e1r4p1.1337.ma>
2022-09-19 18:10:55 +0100

    settings all done

--- frontend/src/components/Pages/Settings/QRZone.tsx
+++ frontend/src/components/Pages/Settings/QRZone.tsx
@@ -1,9 +1,8 @@
 import { useTranslation } from "react-i18next";
 import { ReactComponent as QRIcon } from "../../../assets/icons/QR.svg";
 
-
-const QRZone:React.FC<{
-	isEnabled: boolean
+const QRZone: React.FC<{
+	isEnabled: boolean;
 }> = ({ isEnabled }) => {
 	const { t } = useTranslation();
 
@@ -11,14 +10,18 @@ const QRZone:React.FC<{
 		<section className="container min-w-[300px] rounded-lg flex flex-col justify-center items-center gap-6 text-white  bg-dark-60 bg-opacity-60">
 			<h2 className="text-2xl">{t("settingsPage.EnableTwoFA")}</h2>
 			<div className="relative bg-dark-60 p-2">
-				<QRIcon  />
-				{!isEnabled && <div className="absolute top-0 left-0 h-full w-full bg-dark-60 bg-opacity-80 flex items-center justify-center flex-col text-center">
-					<p className="text-xs w-44">{t("settingsPage.GenerateQR")}</p>
-					<p className="text-8xl font-bold">?</p>
-				</div>}
+				<QRIcon />
+				{!isEnabled && (
+					<div className="absolute top-0 left-0 h-full w-full bg-dark-60 bg-opacity-80 flex items-center justify-center flex-col text-center">
+						<p className="text-xs w-44">
+							{t("settingsPage.GenerateQR")}
+						</p>
+						<p className="text-8xl font-bold">?</p>
+					</div>
+				)}
 			</div>
 		</section>
 	);
-}
+};
 
 export default QRZone;
