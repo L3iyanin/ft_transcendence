659086d38d07544293f7512225894d359c48441c
twelve0 <twelve.abdelali@gmail.com>
2022-09-20 16:27:49 +0100

    add forwardRef to Input custom element

--- frontend/src/components/UI/inputs/Input.tsx
+++ frontend/src/components/UI/inputs/Input.tsx
@@ -1,13 +1,21 @@
+import React from "react";
 
-
-const Input:React.FC<{
-	type: string;
-	className?: string;
-	placeholder?: string;
-}> = ({ placeholder, type, className }) => {
+const Input = React.forwardRef<
+	HTMLInputElement,
+	{
+		type: string;
+		className?: string;
+		placeholder?: string;
+	}
+>(({ placeholder, type, className }, ref) => {
 	return (
-		<input className={`bg-dark-blue rounded-lg py-4 pl-5 w-full text-grey-2 focus:outline-none ${className}`} type={type} placeholder={placeholder} />
+		<input
+			ref={ref}
+			className={`bg-dark-blue rounded-lg py-4 pl-5 w-full text-grey-2 focus:outline-none ${className}`}
+			type={type}
+			placeholder={placeholder}
+		/>
 	);
-}
+});
 
 export default Input;
