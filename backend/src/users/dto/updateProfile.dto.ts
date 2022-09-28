fa247675b14ddcabe1619d7bbdf3cabc29c5965d
twelve0 <twelve.abdelali@gmail.com>
2022-09-28 23:12:34 +0100

    connect achivments logic with gameOver

--- backend/src/users/dto/updateProfile.dto.ts
+++ backend/src/users/dto/updateProfile.dto.ts
@@ -1,14 +1,13 @@
 import { MaxLength } from "@nestjs/class-validator";
 import { ApiProperty } from "@nestjs/swagger";
-import {IsAlpha, IsNotEmpty, IsString } from "class-validator";
+import { IsAlpha, IsNotEmpty, IsString } from "class-validator";
 export class Form {
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    @IsAlpha()
+	@ApiProperty()
+	@IsString()
+	@IsNotEmpty()
+	@IsAlpha()
 	@MaxLength(19, {
-		message: 'UserName is too long, maximun is 19',
-	  })
-    name: string;
+		message: "UserName is too long, maximun is 19",
+	})
+	name: string;
 }
-
