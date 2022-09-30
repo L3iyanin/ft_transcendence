9b5422dc56b459c8736fff4afddb4e43beefca56
Imran Baali <imran.baali19@gmail.com>
2022-09-30 21:20:37 +0100

    fix no channel found ui problem

--- frontend/src/components/Pages/Chat/ConversationsList.tsx
+++ frontend/src/components/Pages/Chat/ConversationsList.tsx
@@ -60,7 +60,7 @@ const ConversationsList: React.FC<{
 					);
 				})}
 				{channels.length === 0 && (
-					<div className="text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-gray-400">
+					<div className="text-center xl:absolute xl:top-1/2 xl:left-1/2 xl:-translate-x-1/2 xl:-translate-y-1/2 text-gray-400">
 						{t("chatPage.noChannels")}
 					</div>
 				)}
